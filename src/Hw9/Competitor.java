fc10ced44fdc1549418bb3cc3d0e7b83901a27bc
Nathan Hsu <nathan333.hsu@gmail.com>
2023-11-12 01:10:24 +0800

    finished homework 9

---
+++ src/Hw9/Competitor.java
@@ -0,0 +1,44 @@
+package Hw9;
+
+public class Competitor implements Runnable {
+
+	int counter = 0;
+	String name;
+
+	public Competitor(String name) {
+		this.name = name;
+	}
+
+	public void run() { // 執行緒執行的地方
+
+		while (counter < 10) {
+			counter++;
+			int sleepTime = (int) (Math.random() * (3000 - 500 + 1)) + 500;
+			System.out.println(name + "吃第" + counter + "碗飯");
+
+			try {
+				Thread.sleep(sleepTime);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(name + "吃完了!");
+	}
+
+	public static void main(String arg[]) {
+		System.out.println("---------------------大胃王快食比賽開始!---------------------");
+		Competitor c1 = new Competitor("饅頭人");
+		Thread t1 = new Thread(c1);
+		Competitor c2 = new Competitor("詹姆士");
+		Thread t2 = new Thread(c2);
+		t1.start();
+		t2.start();
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("---------------------大胃王快食比賽結束!---------------------");
+	}
+}
---
+++ src/Hw9/WaitNotify.java
@@ -0,0 +1,86 @@
+package Hw9;
+
+class Bank {
+	private int money = 0;
+	private int withdrawCount = 0;
+
+	synchronized public void deposit(int qty) {
+		while (money > 3000) {
+			System.out.println("媽媽看到餘額在3000以上,暫停匯款");
+			try {
+				if (withdrawCount < 10) {
+					wait();
+				}
+				break;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		if (money <= 3000) {
+			money += qty;
+			System.out.println("媽媽存了2000,帳戶共有:" + money);
+			notify();
+		}
+	}
+
+	synchronized public void withdraw(int qty) {
+		while (money < qty) {
+			System.out.println("雄大看到帳戶沒錢,暫停提款");
+			try {
+				wait();
+				System.out.println("雄大被老媽告知帳戶以經有錢!");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		money -= qty;
+		withdrawCount++;
+		System.out.println("雄大領了1000,帳戶共有:" + money);
+		if (money < 2000) {
+			System.out.println("雄大看到餘額在2000以下,要求匯款");
+			notify();
+		}
+		else if (withdrawCount >= 10) {
+			notify();
+		}
+	}
+}
+
+class Depositor extends Thread {
+	Bank bank;
+
+	public Depositor(Bank bank) {
+		this.bank = bank;
+	}
+
+	public void run() {
+		for (int i = 1; i <= 10; i++)
+			bank.deposit(2000);
+		System.out.println("媽媽存完了");
+	}
+}
+
+class Withdrawer extends Thread {
+	Bank bank;
+	int count;
+
+	public Withdrawer(Bank bank) {
+		this.bank = bank;
+	}
+
+	public void run() {
+		for (int i = 1; i <= 10; i++)
+			bank.withdraw(1000);
+		System.out.println("雄大領完了");
+	}
+}
+
+public class WaitNotify {
+	public static void main(String[] args) {
+		Bank bank = new Bank();
+		Depositor depositor = new Depositor(bank);
+		Withdrawer withdrawer = new Withdrawer(bank);
+		depositor.start();
+		withdrawer.start();
+	}
+}
